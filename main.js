451dba1e85003dd9be411aae56d9a16d306728e4
ju-lewis <boollewis04@gmail.com>
2024-06-05 02:43:44 +1000

    Start filling out 'about me' section, implement contact modal

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
 
+const modal = document.getElementById("outer-modal");
+
 function openContactModal() {
-	
+	modal.style.display = "block";
 }
 
-function closeContactModal() {
-	
+modal.addEventListener("click", (e) => {
+	if(e.target == modal) {
+		modal.style.display = "none";
+	}
+});
+
+
+/* Scroll functions */
+function scrollToWeb() {
+	const webHeight = document.getElementById("web-dev").getBoundingClientRect().top;
+	window.scrollTo({
+		top: webHeight,
+		left: 0,
+		behavior: "smooth"
+	});
 }
+
+function scrollToML() {
+	const mlHeight = document.getElementById("ml").getBoundingClientRect().top;
+	window.scrollTo({
+		top: mlHeight,
+		left: 0,
+		behavior: "smooth"
+	});
+}
+
+function scrollToED() {
+	const embedHeight = document.getElementById("embedded").getBoundingClientRect().top;
+	window.scrollTo({
+		top: embedHeight,
+		left: 0,
+		behavior: "smooth"
+	});
+}
+
+
